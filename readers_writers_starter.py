c50431f8c9ffe028e593bb3e635841f7cd781f96
linz-ll <210362075@qq.com>
2026-03-30 21:13:55 +0800

    end_write

--- readers_writers_starter.py
+++ readers_writers_starter.py
@@ -114,7 +114,10 @@ class ReadersWritersMonitor:
         """
         with self.condition:
             # TODO: Replace 'pass' with your logic
-            pass
+            self.active_writers -= 1
+            print(f"Writer {writer_id} stops writing")
+
+            self.condition.notify_all()
 
 # Donot Change this
 class Reader(threading.Thread):
