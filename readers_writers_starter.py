3eed838ad755f60ee628eba9a4a39f4d10212831
linz-ll <210362075@qq.com>
2026-03-30 22:17:58 +0800

    wait for all threads to finish

--- readers_writers_starter.py
+++ readers_writers_starter.py
@@ -196,7 +196,8 @@ def main() -> None:
         thread.start()
     
     # TODO: Wait for all threads to finish
-
+    for thread in all_threads:
+        thread.join()
 
     # TODO: Print final message that simulation completed
 
